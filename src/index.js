98252cacc2dcea7240908bbf1c020fb1f8f8ce46
shainasm <smulani2002@google.com>
2021-12-02 20:26:33 +0530

    .

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import connectDB from './services/mongodb/connectDB'
 dotenv.config()
 
 const app = express()
-const port = process.env.PORT || 3002
+const port = process.env.PORT || 3003
 
 connectDB()
 
 app.listen(port, (req, res) => {
-  console.log(`Server listening at PORT ${port}`)
+    console.log(`Server listening at PORT ${port}`)
 })

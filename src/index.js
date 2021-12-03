433b7e4dfa86bd5484585ebe141bcf3340cbf1c8
shainasm <smulani2002@google.com>
2021-12-03 16:39:28 +0530

    setup complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './services/mongodb/connectDB'
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 3000
 
+connectDB()
 
-console.log(process.env.DB_URL)
 
 app.listen(3000, (req, res) => {
     console.log(`Server listening at PORT ${port}`)
--- src/services/mongodb/connectDB.js
+++ src/services/mongodb/connectDB.js
@@ -1,13 +1,14 @@
 import mongoose from 'mongoose'
 
 const connectDB = async () => {
-    const conectionString = process.env.DB_URL
+    const connectionString = process.env.DB_URL
 
     try {
-        await mongoose.connect(conectionString)
+        await mongoose.connect(connectionString)
         console.log('Connected to the DB !!! ')
     } catch (error) {
         console.log(`could not connect to the DB:\n ${error.message}`)
+        console.log(error.message)
     }
 }
 

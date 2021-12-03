862361ec80a89c9cae17fb51e61b590d7a5c8b9e
shainasm <smulani2002@google.com>
2021-12-03 16:22:29 +0530

    set up complete for server corrected

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import connectDB from './services/mongodb/connectDB'
 dotenv.config()
 
 const app = express()
-const port = process.env.PORT || 3003
+const port = process.env.PORT || 3000
 
-connectDB()
 
-app.listen(port, (req, res) => {
+console.log(process.env.DB_URL)
+
+app.listen(3000, (req, res) => {
     console.log(`Server listening at PORT ${port}`)
 })

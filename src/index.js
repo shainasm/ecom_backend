3d8e96e3c7d34784277104d7c14c8fe719898d2e
shainasm <smulani2002@google.com>
2021-12-10 19:02:11 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/category", categoryRoutes)
 app.use("/api/v1/product", productRoutes)
 
-console.log('hii')
+console.log('hi')
 
 app.get('/',(req, res)=> {
     res.send(`Server listening at PORT ${port}`)
